b8e2b619a042ced35be216405660880b154b107d
Valen98 <leo.wahlandtcs@gmail.com>
2024-08-19 11:06:59 +0200

    Added a test for todoItem

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void abstractionTest() {
+        TodoItem todoItem = new TodoItem("A", "B", "C");
 
+        Assertions.assertEquals("A", todoItem.getTitle());
+        Assertions.assertEquals("B", todoItem.getDetail());
+        Assertions.assertEquals("C", todoItem.getStatus());
+    }
 }
